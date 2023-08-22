d61ba4889a09a3572a1b68e30200b351763b4cdc
tangjicheng <tangjch15@gmail.com>
2023-08-22 20:01:03 +0900

    refactor: mv op.go to root dir

--- cmd/remove_pycache.go
+++ cmd/remove_pycache.go
@@ -12,12 +12,13 @@ var removePycacheCmd = &cobra.Command{
 	Short: "a",
 	Long:  "b",
 	Run: func(cmd *cobra.Command, args []string) {
-		errString := "[Command Error] usage: op remove_pycache input_directory"
+		errString := "usage: op remove_pycache input_directory"
 		if len(args) != 1 {
 			fmt.Println(errString)
 			os.Exit(1)
 		}
 		if err := utils.RemoveSpecific(args[0], "__pycache__"); err != nil {
+			fmt.Println(err)
 			fmt.Println(errString)
 			os.Exit(1)
 		}
--- cmd/root.go
+++ cmd/root.go
@@ -1,16 +1,16 @@
 package cmd
 
 import (
+	"fmt"
 	"github.com/spf13/cobra"
 )
 
 var rootCmd = &cobra.Command{
 	Use:   "op",
-	Short: "A tool to remove Python cache files",
-	Long:  "A simple tool to remove .pyc and __pycache__ directories.",
+	Short: "Streamlining daily tasks for seamless computer productivity",
 	Run: func(cmd *cobra.Command, args []string) {
-		// Do nothing if no subcommand is specified
 		if err := cmd.Help(); err != nil {
+			fmt.Println(err)
 			return
 		}
 	},
